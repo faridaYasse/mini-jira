053359e85eab1394f5c8f4de6a60b89eaedddbc6
alihamadamarzouk <alihamadamarzouk@gmail.com>
2026-05-22 19:46:10 +0300

    Serve frontend from backend

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const cors = require('cors');
 const express = require('express');
+const path = require('path');
 
 const usersRouter = require('./src/routes/users');
 const tasksRouter = require('./src/routes/tasks');
@@ -24,6 +25,12 @@ app.use('/api/users', usersRouter);
 app.use('/api/tasks', tasksRouter);
 app.use('/api/projects', projectsRouter);
 app.use('/api/tasks/:taskId/comments', commentsRouter);
+
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
+});
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
